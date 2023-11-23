8b3e2608924c83f806e333dcff93f3a2e67bb5a1
JanineZielman <janine.zielman@icloud.com>
2023-11-23 17:43:34 +0100

    first commit

---
+++ snippets/newsletter.php
@@ -0,0 +1,59 @@
+<div id="mc_embed_shell" class="closer-target">
+      <!-- <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css"> -->
+<!--
+  <style type="text/css">
+        #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
+        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style><style type="text/css">
+        #mc-embedded-subscribe-form input[type=checkbox]{display: inline; width: auto;margin-right: 10px;}
+#mergeRow-gdpr {margin-top: 20px;}
+#mergeRow-gdpr fieldset label {font-weight: normal;}
+#mc-embedded-subscribe-form .mc_fieldset{border:none;min-height: 0px;padding-bottom:0px;}
+</style>-->
+
+<div id="mc_embed_signup">
+
+    <h2 class="newsletter-headline">Subscribe to our Newsletter</h2>
+    <form action="https://sonicacts.us6.list-manage.com/subscribe/post?u=f8f2c0dae61384c2dba08e8ec&amp;id=87c02476f0&amp;v_id=3600&amp;f_id=004733e3f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+        <div id="mc_embed_signup_scroll">
+            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+            <div class="mc-field-group">
+                <label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label>
+
+                <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value="" placeholder="mail@example.com">
+            </div>
+
+            <div id="mergeRow-gdpr" class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
+
+                    <div class="content__gdpr">
+                            <label>Marketing Permissions</label><p>Sonic Acts will use the information you provide on this form to be in touch with you and to provide programme and activity updates as well as marketing. Please let us know all the ways you would like to hear from us:</p>
+
+                        <fieldset class="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
+
+                            <label class="checkbox subfield" for="gdpr1">
+
+                                <input type="checkbox" id="gdpr_1" name="gdpr[1]" class="gdpr" value="Y" checked>
+                                <span>Email</span>
+                            </label>
+
+                        </fieldset>
+
+
+                        <p>You can change your mind at any time by clicking the unsubscribe link in the footer of any email you receive from us, or by contacting us at info@sonicacts.com. We will treat your information with respect. For more information about our privacy practices please visit our website. By clicking below, you agree that we may process your information in accordance with these terms.</p></div><div class="content__gdprLegal"><p>We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your information will be transferred to Mailchimp for processing. <a href="https://mailchimp.com/legal/terms">Learn more</a> about Mailchimp's privacy practices.</p>
+                    </div>
+            </div>
+
+<div hidden=""><input type="hidden" name="tags" value="3375936"></div>
+        <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display: none;"></div>
+            <div class="response" id="mce-success-response" style="display: none;"></div>
+        </div><div aria-hidden="true" style="position: absolute; left: -5000px;"><input type="text" name="b_f8f2c0dae61384c2dba08e8ec_87c02476f0" tabindex="-1" value=""></div><div class="clear"><input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe"></div>
+    </div>
+</form>
+</div>
+<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='MMERGE1';ftypes[1]='text';fnames[2]='MMERGE2';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script></div>
+
+
+<!--
+<div class="ReactModal__Overlay ReactModal__Overlay--after-open" style="position: fixed; inset: 0px; background-color: transparent;"><div class="ReactModal__Content ReactModal__Content--after-open mail-modal" tabindex="-1" role="dialog" aria-modal="true"><div class="close"><svg width="36" height="34" viewBox="0 0 36 34" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="1" y1="-1" x2="44.6296" y2="-1" transform="matrix(0.715187 0.698933 -0.715187 0.698933 1.5 2)" stroke="black" stroke-width="2" stroke-linecap="square"></line><line x1="1" y1="-1" x2="44.6296" y2="-1" transform="matrix(0.715187 -0.698933 0.715187 0.698933 1.5 34)" stroke="black" stroke-width="2" stroke-linecap="square"></line></svg></div><div class="newsletter-subscribe"><div class="d-flex newsletter-input-fields"><div class="mc-field-group"><input type="email" placeholder="Your email" class="mr-2"></div><div class="flex"><div class="info"><p>Sonic Acts will use the information you provide on this form to be in touch with you and to provide programme and activity updates as well as marketing. </p><p>You can change your mind at any time by clicking the unsubscribe link in the footer of any email you receive from us, or by contacting us at info@sonicacts.com. We will treat your information with respect. For more information about our privacy practices please visit our website. By clicking below, you agree that we may process your information in accordance with these terms.</p></div><div class="wp-block-button__link">Submit</div></div></div><div class="newsletter-form-info"></div></div></div></div>-->
